4e3f25fb14446950e18945ec600e40415a6ecb8a
Jefferson Oliveira Lima <oliveiralima.jefferson@gmail.com>
2023-02-21 13:12:49 +0000

    information

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -2,15 +2,17 @@ const AboutContainer = () => {
     return ( 
     <section className="about-container">
         <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quibusdam eveniet possimus exercitationem ipsam,
-             ea quam rem facere aliquid incidunt autem est eligendi accusamus, 
-             id minus aspernatur porro ducimus inventore aliquam!
+        <p>meu nome é Jefferson de Oliveira Lima, 
+        sou estudante de Ciência da Computação CCT College, na Irlanda.
              </p>
-             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quibusdam eveniet possimus exercitationem ipsam,
-             ea quam rem facere aliquid incidunt autem est eligendi accusamus, 
-             id minus aspernatur porro
+             <p>Durante meus estudos, adquiri alguns conhecimentos na programação web, incluindo Javascript, HTML, CSS, Node.js e MySQL. 
+                Também tenho conhecimentos em aplicações web, frameworks, bibliotecas e ferramentas de desenvolvimento. 
+                Por meio de projetos acadêmicos, aprendi a usar métodos ágeis para criar soluções web de qualidade. 
+                Além de minhas habilidades técnicas, tenho uma forte motivação para aprender novas tecnologias. 
+                Estou ansioso para aplicar meus conhecimentos em um ambiente profissional e crescer como desenvolvedor web. 
+                Estou aberto a trabalhar de forma não remunerada e acredito que o meu trabalho pode ser de grande benefício para o seu negócio.
+                Gostaria muito de obter a oportunidade de entrevistar com você para discutir a vaga e como posso contribuir para o seu projeto. 
+
              </p>
     </section>
     );
